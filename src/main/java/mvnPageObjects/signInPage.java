6b4de5511cee613f66ad1060264e821b6e7e194d
JOSE ARTIGA <jrartiga001@gmail.com>
2024-11-11 19:33:15 -0600

    Update signInPage.java

--- src/main/java/mvnPageObjects/signInPage.java
+++ src/main/java/mvnPageObjects/signInPage.java
@@ -12,12 +12,12 @@ WebDriver driver;
 		this.driver = driver;
 	}
 	
-	By email = By.cssSelector("input#field-email");
+	By emailAddress = By.cssSelector("input#field-email");
 	By password = By.cssSelector("input#field-password");
 	By signInButton = By.id("submit-login");
 	
-	public WebElement getEmail() {
-		return driver.findElement(email);
+	public WebElement getEmailAddress() {
+		return driver.findElement(emailAddress);
 	}
 	
 	public WebElement getPassword() {
